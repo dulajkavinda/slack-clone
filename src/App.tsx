77bebfa2bbd113177101162be6c8753d658d6cc1
dulajkavinda <dulajkaavinda@gmail.com>
2021-09-18 19:06:13 +0530

    done

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import { io, Socket } from "socket.io-client";
 
-import Sidebar from "./components/Sidebar";
-
 interface INamespace {
   img: string;
   endpoint: string;
+}
+
+interface IRoom {
+  roomId: number;
+  endpoint: string;
+  roomTitle: string;
+  privateRoom: string;
+  history: Array<String>;
 }
 
 export default function App() {
@@ -14,11 +20,13 @@ export default function App() {
   const [message, setMessage] = useState("");
   const [chat, setChat] = useState<Array<String>>([]);
   const [namespaces, setNamesapces] = useState<INamespace[]>([]);
+  const [rooms, setRooms] = useState<IRoom[]>([]);
+  const [currentNamespace, setCurrentNamespace] = useState<String>("/wiki");
+  const [noOfUsers, setNoOfUsers] = useState<Number>(0);
+  const [currentRoom, setCurrentRoom] = useState<String>("");
 
   const socket_main: Socket = io("http://localhost:8000");
-  const socket_wiki: Socket = io("http://localhost:8000/wiki");
-  const socket_mozilla: Socket = io("http://localhost:8000/mozilla");
-  const socket_linux: Socket = io("http://localhost:8000/linux");
+  const socket_namespaces = io(`http://localhost:8000${currentNamespace}`);
 
   useEffect(() => {
     // let user = prompt("Enter the username: ");
@@ -27,29 +35,77 @@ export default function App() {
     socket_main.on("nsList", (nsData) => {
       setNamesapces(nsData);
     });
-  }, []);
+
+    joinNs();
+
+    socket_namespaces.on("messageToClient", (msg) => {
+      setChat((prevState) => [...prevState, msg]);
+    });
+  }, [currentNamespace]);
+
+  const joinRoom = (room: string) => {
+    socket_namespaces.emit("joinRoom", room, (newNoOfMembers: number) => {});
+    socket_namespaces.on("history", (history) => {
+      setChat(history);
+    });
+    socket_namespaces.on("updateMembers", (newNoOfMembers) => {
+      setNoOfUsers(newNoOfMembers);
+    });
+  };
+
+  const joinNs = () => {
+    socket_namespaces.on("nsRoomLoad", (nsRooms) => {
+      setRooms(nsRooms);
+      joinRoom(nsRooms[0].roomTitle);
+      setCurrentRoom(nsRooms[0].roomTitle);
+    });
+  };
 
   const send = () => {
-    setChat([...chat, message]);
-    console.log("send");
+    const msg = {
+      message: message,
+      roomName: currentRoom,
+    };
+    socket_namespaces.emit("newMessageToServer", msg);
   };
 
   return (
     <div className="main">
-      <Sidebar namespaceList={namespaces} />
+      <div className="namespaces">
+        {namespaces.map((namespace) => {
+          return (
+            <div
+              className="namesapce"
+              onClick={() => setCurrentNamespace(namespace.endpoint)}
+            >
+              <img src={namespace.img} width="95%" height="95%" />
+            </div>
+          );
+        })}
+      </div>
       <div className="rooms">
         <h2>Rooms</h2>
         <div>
           <ul>
-            <li>#room1</li>
-            <li>#room2</li>
-            <li>#room3</li>
-            <li>#room4</li>
+            {rooms.map((room) => {
+              return (
+                <li
+                  onClick={() => {
+                    joinRoom(room.roomTitle);
+                    setCurrentRoom(room.roomTitle);
+                  }}
+                  key={room.roomId}
+                >
+                  {room.roomTitle}
+                </li>
+              );
+            })}
           </ul>
         </div>
       </div>
       <div className="chat">
         <div className="chats">
+          <h3 style={{ marginLeft: "10px" }}>Current Room ({noOfUsers})</h3>
           {chat.map((msg, index) => {
             return (
               <li key={index}>
